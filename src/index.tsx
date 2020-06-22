15a47cc3fb8c14b8490621b30637572fc68ff142
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-22 14:08:40 +1000

    app modularised

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 // 2 minimum packages you need for index file to work as a react app
 import React from "react"
 import ReactDOM from "react-dom"
+import App from "./App"
 
 ReactDOM.render(
-    <h1>Hi There!</h1>,
+    <App />,
     document.querySelector('#root')
 )
