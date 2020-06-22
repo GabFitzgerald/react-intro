d7e6df1fbd1d43eeb0e69d2163a6411414a2970d
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-06-22 11:59:27 +1000

    index.tsx moved to src folder and text rendering to index.hmtl

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
 // 2 minimum packages you need for index file to work as a react app
 import React from "react"
 import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <h1>Hi There!</h1>,
+    document.querySelector('#root')
+)
